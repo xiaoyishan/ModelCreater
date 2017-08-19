7da913fd9df2f9498f62055caff1836052c26a43
xiaoyishan <xiaoyishan1992@163.com>
2017-08-20 00:23:34 +0800

    add frame Extension

--- ModelCreater/ViewController.m
+++ ModelCreater/ViewController.m
@@ -20,8 +20,6 @@
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
 }
 
 - (IBAction)ToModel:(id)sender {
