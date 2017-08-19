0a839ed6c308de136d6b2db4b4d803d8c73a68a6
xiaoyishan <xiaoyishan1992@163.com>
2017-08-20 02:11:15 +0800

    add note enum

--- ModelCreater/JsonFormatToModel.h
+++ ModelCreater/JsonFormatToModel.h
@@ -7,16 +7,34 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Cocoa/Cocoa.h>
+
+typedef NS_ENUM(NSUInteger, NoteType) {
+    NoteTypeDefault ,       // "//"
+    NoteTypeDocument,       // "/**/"
+};
+typedef NS_ENUM(NSUInteger, NoteDirection) {
+    NoteDirectionTop ,      // "↑"
+    NoteDirectionRight,     // "→"
+};
+typedef NS_ENUM(NSUInteger, NoteLanguage) {
+    NoteLanguageDefault ,       // 简体中文
+    NoteLanguageZHhant,         // 繁體中文
+    NoteLanguageJapanese,       // 日本語
+    NoteLanguageHindi,          // 印度語
+    NoteLanguageFrench,         // 法語
+    NoteLanguageRussian,        // 俄語
+    NoteLanguageKorean,         // 韩語
+};
+
 
 @interface JsonFormatToModel : NSObject{
     NSString *ModelStr;
     NSString *ViewStr;
 }
 
-
--(NSString*)TranslateToModelCode:(NSString*)json RootClassName:(NSString*)className;// ->model
-
--(NSString*)TranslateToViewCode:(NSString*)json HasIB:(BOOL)IB;// ->view
+// ->model
+-(NSString*)TranslateToModelCode:(NSString*)json RootClassName:(NSString*)className;
+// ->view
+-(NSString*)TranslateToViewCode:(NSString*)json HasIB:(BOOL)IB;
 
 @end
