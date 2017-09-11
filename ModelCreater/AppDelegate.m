01b3dad68ebf64c28ca6e33e7aaa77c041075d63
xiaoyishan <xiaoyishan1992@163.com>
2017-09-11 09:48:22 +0800

    renote

--- ModelCreater/AppDelegate.m
+++ ModelCreater/AppDelegate.m
@@ -28,52 +28,52 @@
 }
 
 
-- (void)applicationWillFinishLaunching:(NSNotification *)notification{
-
-}
-
-- (void)applicationWillHide:(NSNotification *)notification{
-
-}
-- (void)applicationDidHide:(NSNotification *)notification{
-
-}
-- (void)applicationWillUnhide:(NSNotification *)notification{
-
-}
-- (void)applicationDidUnhide:(NSNotification *)notification{
-
-}
-- (void)applicationWillBecomeActive:(NSNotification *)notification{
-
-}
-- (void)applicationDidBecomeActive:(NSNotification *)notification{
-
-}
+//- (void)applicationWillFinishLaunching:(NSNotification *)notification{
+//
+//}
+//
+//- (void)applicationWillHide:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidHide:(NSNotification *)notification{
+//
+//}
+//- (void)applicationWillUnhide:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidUnhide:(NSNotification *)notification{
+//
+//}
+//- (void)applicationWillBecomeActive:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidBecomeActive:(NSNotification *)notification{
+//
+//}
 - (void)applicationWillResignActive:(NSNotification *)notification{
     [self.RootWindow makeKeyAndOrderFront:self];
 }
 
 
-
-- (void)applicationDidResignActive:(NSNotification *)notification{
-
-}
-- (void)applicationWillUpdate:(NSNotification *)notification{
-
-}
-- (void)applicationDidUpdate:(NSNotification *)notification{
-
-}
-- (void)applicationWillTerminate:(NSNotification *)notification{
-
-}
-- (void)applicationDidChangeScreenParameters:(NSNotification *)notification{
-
-}
-- (void)applicationDidChangeOcclusionState:(NSNotification *)notification{
-
-}
+//
+//- (void)applicationDidResignActive:(NSNotification *)notification{
+//
+//}
+//- (void)applicationWillUpdate:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidUpdate:(NSNotification *)notification{
+//
+//}
+//- (void)applicationWillTerminate:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidChangeScreenParameters:(NSNotification *)notification{
+//
+//}
+//- (void)applicationDidChangeOcclusionState:(NSNotification *)notification{
+//
+//}
 
 
 @end
